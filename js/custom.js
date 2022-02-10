f47765d6d01cbe03b6a68ee79ef227d7e7593191
Chris <weeblytricks@gmail.com>
2022-02-09 23:10:58 -0500

    changes

--- js/custom.js
+++ js/custom.js
@@ -341,7 +341,8 @@ jQuery(function($) {
 	// Square Feet to Acre Conversion
 	if ($('.mowingWeekly').hasClass('green') && $('.squareFeetCalculate').hasClass('green') && (inputValue > 0)) {
       $(".acreCalculate").on("click", function(){
-	    alert("Acre Clicked! Time to Convert.") 
+	    var convertedInputValue = (inputValue * 43560);
+		$(".calculatorInput").val(convertedInputValue);
 	  });
 	}
 	
